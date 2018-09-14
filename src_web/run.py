1a2ee6a4b0e6fae55785ede4f9832b4dd0c283f5
Egor Panfilov <egor.panfilov@oulu.fi>
2018-09-14 13:23:38 +0000

    Added frontend serving on Flask

---
+++ src_web/run.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, send_from_directory
+
+
+app = Flask(__name__, static_folder='build')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists("build/" + path):
+        return send_from_directory('build', path)
+    else:
+        return send_from_directory('build', 'index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='mipt-ml.oulu.fi', use_reloader=False, debug=True, port=5001, threaded=True)
