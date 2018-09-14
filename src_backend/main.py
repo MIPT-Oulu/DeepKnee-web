57a2b3ff898fcd2ec0e47bf0b6be6144d6b1e12c
Egor Panfilov <egor.v.panfilov@gmail.com>
2018-09-14 18:59:11 +0300

    Working backend

--- src_backend/main.py
+++ src_backend/main.py
@@ -10,7 +10,6 @@ import matplotlib
 matplotlib.use('Agg')
 import imageio
 import numpy as np
-import torch
 
 import socketio
 import eventlet
@@ -43,22 +42,20 @@ config = parse_args()
 
 class KneeWrapper(object):
     def __init__(self):
-        self._kneeloc = KneeLocalizer()
+        self.kneeloc = KneeLocalizer()
 
         nets_snapshots_names = []
         for snp in SNAPSHOTS_EXPS:
             nets_snapshots_names.extend(
                 glob(os.path.join(SNAPSHOTS_KNEE_GRADING, snp, '*.pth')))
-        self._deepknee = KneeNetEnsemble(
+        self.deepknee = KneeNetEnsemble(
             snapshots_paths=nets_snapshots_names,
             mean_std_path=os.path.join(SNAPSHOTS_KNEE_GRADING, 'mean_std.npy'))
         del nets_snapshots_names
 
     def run(self, path_raw, path_crop, path_inf):
-        # Find all DICOM files in the provided path
-        fnames = glob(os.path.join(path_raw, '*'))
-        fnames = [os.path.basename(e) for e in fnames]
-        fname = fnames[0]
+        # Find the first DICOM file in the provided path
+        fname = glob(os.path.join(path_raw, '*'))[0]
 
         # Read and preprocess DICOM
         tmp = read_dicom(fname)
@@ -69,27 +66,32 @@ class KneeWrapper(object):
             logger.error('Invalid results of DICOM reading')
             return False
         img, spacing = tmp
-        img = preprocess_xray(img)
+        img_prep = preprocess_xray(img)
+        logger.info('DICOM read')
 
         # Knee localization
-        det_l, det_r = self._kneeloc.predict(fileobj=img, spacing=spacing)
+        det_l, det_r = self.kneeloc.predict(fileobj=img_prep,
+                                            spacing=spacing)
         det_both = det_l + det_r
-        logger.debug('KneeLocalizer results:\n{}, {}'
-                     .format(repr(det_l), repr(det_r)))
+        logger.info('Localization finished')
 
         # Image cropping
         knee_l, knee_r = process_file_or_image(
-            image=None, spacing=spacing,
+            image=img.astype(np.float64), spacing=spacing,
             save_dir=path_crop, bbox=det_both, gradeL=5, gradeR=5,
-            sizemm=140, pad=300, save_vis=True)
+            save_vis=True)
+        logger.info('Cropping finished')
 
         # Knee grading
-        self._deepknee.predict_save(fileobj_in=knee_l, nbits=16,
-                                    fname_suffix='0',
-                                    path_dir_out=path_inf)
-        self._deepknee.predict_save(fileobj_in=knee_r, nbits=16,
-                                    fname_suffix='1',
-                                    path_dir_out=path_inf)
+        self.deepknee.predict_save(fileobj_in=knee_l, nbits=16,
+                                   fname_suffix='0',
+                                   path_dir_out=path_inf,
+                                   fliplr=True)
+        logger.info('Grading L finished')
+        self.deepknee.predict_save(fileobj_in=knee_r, nbits=16,
+                                   fname_suffix='1',
+                                   path_dir_out=path_inf)
+        logger.info('Grading R finished')
 
 
 knee_wrapper = KneeWrapper()
@@ -119,52 +121,55 @@ class SIONamespace(socketio.Namespace):
 
     def on_dicom_submission(self, sid, data):
         # logger.debug('Received message: {}'.format(data))
+        logger.info('Message received')
 
         global knee_wrapper
 
-        if True:
-            with tempfile.TemporaryDirectory() as tmp_dir:
-                # Create subfolders to store intermediate results
-                path_raw = os.path.join(tmp_dir, '00_raw')
-                path_crop = os.path.join(tmp_dir, '01_crop')
-                path_inf = os.path.join(tmp_dir, '02_inf')
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            # logger.debug(f'tmp dir: {tmp_dir}')
+            # Create subfolders to store intermediate results
+            path_raw = os.path.join(tmp_dir, '00_raw')
+            path_crop = os.path.join(tmp_dir, '01_crop')
+            path_inf = os.path.join(tmp_dir, '02_inf')
 
-                for p in (path_raw, path_crop, path_inf):
-                    os.makedirs(p)
+            for p in (path_raw, path_crop, path_inf):
+                os.makedirs(p)
 
-                # Receive and decode DICOM image
-                fname_dicom = os.path.join(path_raw, 'image.dicom')
-                with open(fname_dicom, 'wb') as f:
-                    # Remove the web-content prefix
-                    tmp = data['file_blob'].split(',', 1)[1]
-                    f.write(base64.b64decode(tmp))
+            # Receive and decode DICOM image
+            fname_dicom = os.path.join(path_raw, 'image.dicom')
+            with open(fname_dicom, 'wb') as f:
+                # Remove the web-content prefix
+                tmp = data['file_blob'].split(',', 1)[1]
+                f.write(base64.b64decode(tmp))
 
-                # Run knee localization and grading
-                knee_wrapper.run(
-                    path_raw=path_raw,
-                    path_crop=path_crop,
-                    path_inf=path_inf
-                )
+            logger.info('Message decoded')
 
-                # Find the files with the results
-                paths_results_raw = list(sorted(
-                    glob(os.path.join(path_crop, '**', '*.png'))))
-                paths_results_heatmap = list(sorted(
-                    glob(os.path.join(path_inf, 'heatmap_*.png'))))
-                paths_results_prob = list(sorted(
-                    glob(os.path.join(path_inf, 'prob_*.png'))))
+            # Run knee localization and grading
+            knee_wrapper.run(
+                path_raw=path_raw,
+                path_crop=path_crop,
+                path_inf=path_inf
+            )
 
-                # Pack the results into JSON-message
-                # TODO: implement image_src acquisition
-                # "image_src": _png_to_web_base64(paths_results_heatmap[0]),
-                ret = {
-                    "image_1st_raw": _png_to_web_base64(paths_results_raw[0], fliplr=True),
-                    "image_2nd_raw": _png_to_web_base64(paths_results_raw[1]),
-                    "image_1st_heatmap": _png_to_web_base64(paths_results_heatmap[0], fliplr=True),
-                    "image_2nd_heatmap": _png_to_web_base64(paths_results_heatmap[1]),
-                    "special_1st": _png_to_web_base64(paths_results_prob[0]),
-                    "special_2nd": _png_to_web_base64(paths_results_prob[1])
-                }
+            # Find the files with the results
+            paths_results_raw = list(sorted(
+                glob(os.path.join(path_crop, '**', '*.png'))))
+            paths_results_heatmap = list(sorted(
+                glob(os.path.join(path_inf, 'heatmap_*.png'))))
+            paths_results_prob = list(sorted(
+                glob(os.path.join(path_inf, 'prob_*.png'))))
+
+            # Pack the results into JSON-message
+            # TODO: implement image_src acquisition
+            # "image_src": _png_to_web_base64(paths_results_heatmap[0]),
+            ret = {
+                "image_1st_raw": _png_to_web_base64(paths_results_raw[0], fliplr=True),
+                "image_2nd_raw": _png_to_web_base64(paths_results_raw[1]),
+                "image_1st_heatmap": _png_to_web_base64(paths_results_heatmap[0], fliplr=True),
+                "image_2nd_heatmap": _png_to_web_base64(paths_results_heatmap[1]),
+                "special_1st": _png_to_web_base64(paths_results_prob[0]),
+                "special_2nd": _png_to_web_base64(paths_results_prob[1])
+            }
 
         # path_debug = '/Users/egor/Desktop/Screen Shot 2018-09-01 at 19.59.40.png'
         # ret = json.dumps(
@@ -192,4 +197,5 @@ sio.register_namespace(SIONamespace())
 app = socketio.Middleware(sio, app)
 
 # Deploy as an eventlet WSGI server
-eventlet.wsgi.server(eventlet.listen(('mipt-ml.oulu.fi', 5000)), app)
+eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+# eventlet.wsgi.server(eventlet.listen(('mipt-ml.oulu.fi', 5000)), app)
