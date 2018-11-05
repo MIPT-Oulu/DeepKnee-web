fa38e65e8c8ed74e0f924a68c7d6eadef8bea479
Egor Panfilov <egor.v.panfilov@gmail.com>
2018-11-05 23:39:12 +0200

    Refrain from using screen in the main script

--- run_service.sh
+++ run_service.sh
@@ -1,16 +1,20 @@
-for scr in $(ls /tmp/uscreens/S-$USER)
-do
-    screen -X -S $scr quit
-done
+#for scr in $(ls /tmp/uscreens/S-$USER)
+#do
+#    screen -X -S $scr quit
+#done
 
-for scr in $(ls  /run/screen/S-$USER)
-do
-    screen -X -S $scr quit
-done
+#for scr in $(ls  /run/screen/S-$USER)
+#do
+#    screen -X -S $scr quit
+#done
 
 cd src_web
 npm install
 npm run build
 cd ..
 
-screen -dmS deepknee_app && screen -S deepknee_app -X stuff "source activate deep_knee_web && cd src_web && python run_service.py 2>&1\n"
+source activate deep_knee_web
+cd src_web
+python run_service.py
+
+#screen -dmS deepknee_app && screen -S deepknee_app -X stuff "source activate deep_knee_web && cd src_web && python run_service.py 2>&1\n"
